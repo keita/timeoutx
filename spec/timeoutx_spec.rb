5e064ba884de4b33d2ca22dcfd3211425fc047b8
Keita Yamaguchi <keita@BlackCoffee.(none)>
2008-04-09 18:36:03 +0900

    Moved from rspec to bacon

--- spec/timeoutx_spec.rb
+++ spec/timeoutx_spec.rb
@@ -1,21 +1,17 @@
-require File.dirname(__FILE__) + '/spec_helper.rb'
-$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "lib")
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..", "lib")
 require "timeoutx"
+require "bacon"
 
-describe "timeout" do
+describe "TimeoutX" do
   it "should be timeout" do
     Proc.new do
-      1.upto(100000) do
-        TimeoutX.timeout(0.1){sleep 0.09}
-      end
-    end.should raise_error(TimeoutX::Error)
+      TimeoutX.timeout(0.1){sleep 1}
+    end.should.raise(TimeoutX::Error)
   end
 
   it "should not be timeout" do
     Proc.new do
-      1.upto(100000) do
-        TimeoutX.timeout(0.09){sleep 0.1}
-      end
-    end.should_not raise_error(TimeoutX::Error)
+      TimeoutX.timeout(1){sleep 0.1}
+    end.should.not.raise(TimeoutX::Error)
   end
 end
