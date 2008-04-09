c4d0e52d189e9a0580390770b77bb54a4be2bde1
Keita Yamaguchi <keita.yamaguchi@gmail.com>
2008-04-10 02:39:43 +0900

    lib/timeoutx.rb: Added version information

--- lib/timeoutx.rb
+++ lib/timeoutx.rb
@@ -1,6 +1,16 @@
 require 'singleton'
 
 class TimeoutX
+  module Version #:nodoc:
+    MAJOR = 0
+    MINOR = 2
+    TINY  = 1
+
+    STRING = [MAJOR, MINOR, TINY].join('.')
+  end
+
+  VERSION = Version::STRING
+
   include Singleton
 
   attr_accessor :interval
