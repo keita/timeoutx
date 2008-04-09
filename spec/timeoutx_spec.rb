2cdc0ee7a4c32d8fae3ac521ee8371494eaba39a
Keita Yamaguchi <keita.yamaguchi@gmail.com>
2008-04-09 18:50:27 +0900

    Revert "Moved from rspec to bacon"
    This reverts commit 5e064ba884de4b33d2ca22dcfd3211425fc047b8.

--- spec/timeoutx_spec.rb
+++ spec/timeoutx_spec.rb
@@ -1,17 +1,21 @@
-$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..", "lib")
+require File.dirname(__FILE__) + '/spec_helper.rb'
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "lib")
 require "timeoutx"
-require "bacon"
 
-describe "TimeoutX" do
+describe "timeout" do
   it "should be timeout" do
     Proc.new do
-      TimeoutX.timeout(0.1){sleep 1}
-    end.should.raise(TimeoutX::Error)
+      1.upto(100000) do
+        TimeoutX.timeout(0.1){sleep 0.09}
+      end
+    end.should raise_error(TimeoutX::Error)
   end
 
   it "should not be timeout" do
     Proc.new do
-      TimeoutX.timeout(1){sleep 0.1}
-    end.should.not.raise(TimeoutX::Error)
+      1.upto(100000) do
+        TimeoutX.timeout(0.09){sleep 0.1}
+      end
+    end.should_not raise_error(TimeoutX::Error)
   end
 end
