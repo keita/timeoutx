fdaa556852f6140a207e3b85c5df8858c8de357e
Keita Yamaguchi <keita.yamaguchi@gmail.com>
2008-04-10 19:26:53 +0900

    version 0.3.0

--- lib/timeoutx.rb
+++ lib/timeoutx.rb
@@ -3,8 +3,8 @@ require 'singleton'
 class TimeoutX
   module Version #:nodoc:
     MAJOR = 0
-    MINOR = 2
-    TINY  = 1
+    MINOR = 3
+    TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
